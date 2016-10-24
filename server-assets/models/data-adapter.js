89ded197ee26b34df04b7ee31527ff2fd6ef866c
Jonathan Steenblik <blikone@hotmail.com>
2016-10-24 12:10:10 -0600

    Bring level with classroom example

--- server-assets/models/data-adapter.js
+++ server-assets/models/data-adapter.js
@@ -1,19 +1,16 @@
-; (function () {
-
-  let uuid = require('node-uuid'),
+let uuid = require('node-uuid'),
     JsData = require('js-data'),
     Schemator = require('js-data-schema'),
     NeDbAdapter = require('js-data-nedb'),
     schemator = new Schemator(),
     DS = new JsData.DS();
 
-  DS.registerAdapter('nedb', NeDbAdapter, { default: true })
+DS.registerAdapter('nedb', NeDbAdapter, { default: true })
+//This one line tells the whole server to use NeDB as its default database.
+//To change the database service, add a dependency and change this one line.
 
-  module.exports = {
+module.exports = {
     DS,
     uuid,
     schemator
-  }
-
-
-} ());
+}
--- server-assets/models/galaxy-model.js
+++ server-assets/models/galaxy-model.js
@@ -1,36 +1,45 @@
-; (function () {
-
-  let dataAdapter = require('./data-adapter'),
+let dataAdapter = require('./data-adapter'),
     uuid = dataAdapter.uuid,
     schemator = dataAdapter.schemator,
     DS = dataAdapter.DS;
 
-  let Galaxy = DS.defineResource({
+let Galaxy = DS.defineResource({
     name: 'galaxy',
-		endpoint: 'galaxies',
+    endpoint: 'galaxies',
     filepath: __dirname + '/../data/galaxies.db',
-    relations: {}
-  })
+    relations: {
+        hasMany: {
+            star: {
+                localField: 'stars',
+                foreignKey: 'galaxyId'
+            }
+        }
+    }
+})
 
-  function create(name, cb) {
+function create(name, cb) {
     // Use the Resource Model to create a new galaxy
-    Galaxy.create({id: uuid.v4(), name: name}).then(cb).catch(cb)
-  }
+    Galaxy.create({ id: uuid.v4(), name: name }).then(cb).catch(cb)
+}
 
-  function getAll(query,cb) {
+function getAll(query, cb) {
     //Use the Resource Model to get all Galaxies
-    Galaxy.findAll({}).then(cb)
-  }
+    Galaxy.findAll({}).then(cb).catch(cb)
+}
 
-  function getById(id, query, cb){
+function getById(id, query, cb) {
     // use the Resource Model to get a single galaxy by its id
-    Galaxy.find(id).then(cb).catch(cb)
-  }
+    Galaxy.find(id, formatQuery(query)).then(cb).catch(cb)
+}
 
-  module.exports = {
+function formatQuery(query) {
+    return {
+        with: query.split(',').join(' ').split(' ')
+    }
+}
+
+module.exports = {
     create,
     getAll,
     getById
-  }
-
-} ());
+}
